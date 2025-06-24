22a0389661d05d92eb9f7a3a0b03b0969cfade1a
Sabaa Assi <sabaa.assi2911@gmail.com>
2025-06-24 10:27:57 +0300

    Update Docker setup and remove old Spark jobs

--- orchestration/spark_jobs/bronze_to_silver.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("Bronze to Silver").getOrCreate()
-
-df = spark.read.csv("/data/bronze/input_data.csv", header=True, inferSchema=True)
-df_clean = df.dropna()
-df_clean.write.mode("overwrite").parquet("/data/silver/cleaned_data.parquet")
-
-spark.stop()
